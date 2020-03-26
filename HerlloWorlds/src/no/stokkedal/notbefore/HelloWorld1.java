129d8ec99d72ace79bad82d8737173a9e034fcb7
Tore Stokkedal <tore.stokkedal@ibm.com>
2020-03-26 15:33:13 +0100

    Created Hello World.

---
+++ HerlloWorlds/src/no/stokkedal/notbefore/HelloWorld1.java
@@ -0,0 +1,14 @@
+package no.stokkedal.notbefore;
+
+public class HelloWorld1 {
+
+	public HelloWorld1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
